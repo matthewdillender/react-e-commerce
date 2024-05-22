f77fab88576436f3fc6fdb047c5411cb1dbeb4d5
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-05-22 09:42:28 -0700

    add navbar

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,9 @@ import swanBio from "../images/swanBio.jpeg";
 export function HomePage() {
   return (
     <main>
-      <h1>Welcome to The Ecommerce Store</h1>
+      <h1>Welcome to Fire Swan Pottery</h1>
       <img src={swanBio} alt="my image" />
+      <p>My journey as a potter and devoted iterest in...</p>
     </main>
   );
 }
